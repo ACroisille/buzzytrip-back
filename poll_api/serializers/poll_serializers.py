50daf4be57ccffc84a876fcc87d728681806498d
antoinecroisille <antoine.croisille@glady.com>
2022-12-02 12:40:35 +0100

    start_date > end_date constraint added

--- poll_api/serializers/poll_serializers.py
+++ poll_api/serializers/poll_serializers.py
@@ -15,6 +15,17 @@ class PollListSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'start_date', 'end_date', 'closing_time', 'participants', 'created_by',
                   'pseudo']
 
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        if start_date and end_date and start_date > end_date:
+            raise serializers.ValidationError('Finish must occur after start')
+
+        return data
+
     def create(self, validated_data):
         """
         Poll object creation
@@ -24,9 +35,8 @@ class PollListSerializer(serializers.ModelSerializer):
         pseudo = validated_data.pop('pseudo', None)
         poll = Poll.objects.create(**validated_data)
         pseudo = pseudo if pseudo else poll.created_by.username
-        Participant.objects.create(poll=poll, user=poll.created_by, pseudo=pseudo)
-
-        # TODO : Add constraint start_date > end_date
+        Participant.objects.create(
+            poll=poll, user=poll.created_by, pseudo=pseudo)
 
         return poll
 
@@ -38,4 +48,5 @@ class PollDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Poll
-        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'closing_time', 'participants', 'created_by']
+        fields = ['id', 'name', 'description', 'start_date',
+                  'end_date', 'closing_time', 'participants', 'created_by']
